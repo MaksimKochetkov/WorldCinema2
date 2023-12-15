5d7e5e0bbac57da014cb7ebd87efaab949aca8c0
pro100chelovek33 <Zz428159@gmail.com>
2023-12-15 16:00:03 +0500

    snmicke

---
+++ app/src/main/java/com/example/usol_action/magicians.kt
@@ -0,0 +1,11 @@
+package com.example.usol_action
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class magicians : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_magicians)
+    }
+}
--- app/src/main/java/com/example/usol_action/user.kt
+++ app/src/main/java/com/example/usol_action/user.kt
@@ -1,45 +1,24 @@
 package com.example.usol_action
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.MediaController
-import android.widget.TextView
 import android.widget.Toast
-import android.widget.VideoView
 import androidx.appcompat.widget.Toolbar
-import androidx.recyclerview.widget.RecyclerView
 
-
-class back : AppCompatActivity() {
+class user : AppCompatActivity() {
     lateinit var tb: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_back)
+        setContentView(R.layout.activity_user)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         tb=findViewById(R.id.toolbar)
         setSupportActionBar(this.tb)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title="Star Wars"
-
-        val news: RecyclerView = findViewById((R.id.charrecycle))
-        news.adapter = char_adapter(this, char_list().list)
-        val cadr: RecyclerView = findViewById((R.id.cadrrecycler))
-        cadr.adapter = cadr_adapter(this, cadr_list().list)
-
-
-
+        supportActionBar?.title="Пользователь"
     }
-
     override fun onOptionsItemSelected (item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
@@ -68,6 +47,4 @@ class back : AppCompatActivity() {
         menuInflater.inflate(R.menu.menushka, menu)
         return true
     }
-
-
 }
